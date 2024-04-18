a4cf6258515ce8afbff29a74cea166c4108dc8ef
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-04-18 14:37:25 -0500

    Add files via upload

---
+++ Animations/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+    Student Name: MrJ
+	File Name: script.js
+	Current Date: 4/18/2024
+*/
+
+// Function to display content
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created \
+    with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
---
+++ jQueryIntro/scripts/script.js
@@ -0,0 +1,16 @@
+/*
+    Student Name: MrJ
+    File Name: script.js
+    Date: 4/18/2024
+*/
+
+// Hide paragraphs
+$( "#hide-btn" ).on( "click", function() {
+    $( "p" ).hide( "slow" );
+  });
+  
+// Show paragraphs
+$( "#show-btn" ).on( "click", function() {
+    $( "p" ).show( "slow" );
+  });
+  
