bc1e69a89f5031b5b0429a38ea2b6415d099a176
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-05-02 12:25:05 -0500

    Add files via upload

---
+++ EldenRingBosses/scripts/script.js
@@ -0,0 +1,19 @@
+/*
+    Student Name: MrJ
+    File Name: script.js
+    Date: 4/29/2024
+*/
+
+// Hide paragraphs
+$( "#radahn-btn" ).on( "click", function() {
+    $( "p1" ).hide( "slow" );
+  });
+  
+  $( "#rykard-btn" ).on( "click", function() {
+    $( "p2" ).hide( "slow" );
+  });
+
+  $( "#malenia-btn" ).on( "click", function() {
+    $( "p3" ).hide( "slow" );
+  });
+
